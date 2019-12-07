75b90b8dcbcb8a5a55622d950d0e8a1a38fbeac3
reshinto <terencekong2002@gmail.com>
2019-12-07 16:25:49 +0800

    Updates

---
+++ precache-manifest.3ffb2c58728cdfa674dbaec17558643e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69b2f54c74b33713ea399c58fff8829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe2515f303dd55f717e8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9d1305842bbf8690fe0",
+    "url": "/static/js/2.ac7389bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ac7389bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe2515f303dd55f717e8",
+    "url": "/static/js/main.b04cec42.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

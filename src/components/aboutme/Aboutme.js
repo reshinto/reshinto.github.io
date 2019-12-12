f41cb3ffa1ffd369aa7b0376f23e411b52ce5943
reshinto <terencekong2002@gmail.com>
2019-12-12 19:20:39 +0800

    Add bookmarklet

--- src/components/aboutme/Aboutme.js
+++ src/components/aboutme/Aboutme.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "@material-ui/core/Button"
 
 let i = 0;
 
@@ -52,31 +53,48 @@ class Aboutme extends React.Component {
     }
   }
 
+  activate = () => {
+    // let s, ss = ["http://kathack.com/js/kh.js","http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"];
+    // for (let i=0; i!=ss.length; i++) {
+    //   s = document.createElement("script");
+    //   s.src = ss[i];
+    //   document.body.appendChild(s);
+    // }
+    // void(0);
+    const s = document.createElement("script");
+    s.type="text/javascript";
+    document.body.appendChild(s);
+    s.src="//hi.kickassapp.com/kickass.js";
+    void(0);
+
+  }
+
   render() {
     return (
-      <div style={{margin: "50px"}}>
-        <h1 style={{textAlign: "center", color: "#CFCFCF"}}>
+      <div style={{margin: "50px", textAlign: "center", color: "#CFCFCF"}}>
+        <h1>
           Hey! I am Terence Kong
         </h1>
-        <h2 style={{textAlign: "center", color: "#CFCFCF"}}>I am a {this.state.show}<span ref="word" className="blink">|</span></h2>
-        <p style={{textAlign: "center", color: "#CFCFCF", margin: "2rem 4rem"}}>
+        <h2>I am a {this.state.show}<span ref="word" className="blink">|</span></h2>
+        <p style={{margin: "2rem 4rem"}}>
           Self-taught software engineer who is passionate about devising elegant problem-solving methods on top of learning new technologies and tools.
         </p>
-        <h3 style={{textAlign: "center", color: "#CFCFCF"}}>
+        <h3>
           How it all began.
         </h3>
-        <p style={{textAlign: "center", color: "#CFCFCF", margin: "2rem 4rem"}}>
+        <p style={{margin: "2rem 4rem"}}>
           Software engineering has always been an interest of mine. I wrote my first line of code in C++ during polytechnic (High School equivalent) and had graduated with a Diploma in Electronics, Computer, and Communications Engineering.
         </p>
-        <p style={{textAlign: "center", color: "#CFCFCF", margin: "2rem 4rem"}}>
+        <p style={{margin: "2rem 4rem"}}>
           After that, I wanted to acquire a broad and varied knowledge of the arts and sciences, which led me to pursue an International Liberal Arts Degree.
         </p>
-        <p style={{textAlign: "center", color: "#CFCFCF", margin: "2rem 4rem"}}>
+        <p style={{margin: "2rem 4rem"}}>
           During my job experience, I had witnessed the usefulness of programming, especially in the area of workflow automation. Wanting to know more, I delve into expanding my understanding of software engineering, which led me to discover new technologies such as mobile, data science, web, VR / AR, and many more.
         </p>
-        <p style={{textAlign: "center", color: "#CFCFCF", margin: "2rem 4rem"}}>
+        <p style={{margin: "2rem 4rem"}}>
           In all the experiences I went through have cemented my interest in craving the ability to create applications of my own.
         </p>
+        <Button variant="contained" color="secondary" onClick={this.activate}>Destroy website!</Button>
       </div>
     );
   }

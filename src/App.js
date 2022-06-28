6511e07d9e8e587f45e1fe6bd3f0683f9be45174
reshinto <terencekong2002@gmail.com>
2022-06-28 16:56:08 +0800

    Temporarily add redirection to linkedin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import {Provider} from "react-redux";
+import {BrowserRouter as Router} from "react-router-dom";
 import Routes from "./Routes";
 import store from "./redux/store";
 import Header from "./components/headers/header";
 
 function App() {
+  window.location.href = "https://sg.linkedin.com/in/terence-kong";
   return (
     <Provider store={store}>
       <Router>
